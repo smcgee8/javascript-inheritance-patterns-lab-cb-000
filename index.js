ad4a6bb3a60c5f0c599926d27016a42f9e90e19f
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-30 00:17:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function Point(x,y) {
 function Shape() {
   this.addToPlane = function(x,y) {
     this.position = new Point(x,y);
-  }
+  };
   this.move = function(x,y) {
     this.position.x = x;
     this.position.y = y;
