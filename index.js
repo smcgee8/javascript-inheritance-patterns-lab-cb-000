f896bd78c0cf66e29ae38638832a3eb84d2568b6
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-30 00:32:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,15 +7,7 @@ Point.prototype.toString = function() {
   return "(" + this.x + ", " + this.y + ")";
 }
 
-function Shape() {
-  this.addToPlane = function(x,y) {
-    this.position = new Point(x,y);
-  }
-  this.move = function(x,y) {
-    this.position.x = x;
-    this.position.y = y;
-  }
-}
+function Shape() {}
 
 Shape.prototype.addToPlane = function(x,y) {
   this.position = new Point(x,y);
